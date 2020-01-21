249ebc3430e8cf5f089ab0218cf227abead4d3b6
Maria Pensativa <mariya.pensativa@gmail.com>
2020-01-21 19:16:01 +0200

    Добавляет вторую форму обратной связи. Мелкие правки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
+//Прокрутка к якорю меню
+
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+var animationTime = 500;
+var framesCount = 20;
+
+anchors.forEach(function(item) {
+  item.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    let scroller = setInterval(function() {
+      let scrollBy = coordY / framesCount;
+
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  });
+});
+
 //Callback popap
 
-const openPopap = document.querySelector('.button--catalog');
-const closePopap = document.querySelector('.callback__clouse');
-const popap = document.querySelector('.callback--contacts');
+var openPopap = document.querySelector('.button--catalog');
+var closePopap = document.querySelector('.callback__clouse--contacts');
+var popap = document.querySelector('.callback--contacts');
 
 popap.classList.remove('open');
 
@@ -16,60 +41,19 @@ closePopap.addEventListener('click', function(evet) {
   popap.classList.remove('open');
 });
 
-/*
-jQuery(function ($) {
-  $(function(){
-    $('.button--catalog').on('click', function(evet){
-      evet.preventDefault();
-      $('.callback--contacts').addClass('open');
-    });
+var openPopapTwo = document.querySelector('.button--review');
+var closePopapTwo = document.querySelector('.callback__clouse--review');
+var popapTwo = document.querySelector('.callback--review');
 
-    $('.callback__clouse').on('click', function(evet){
-      evet.preventDefault();
-      $('.callback--contacts').removeClass('open');
-    });
-  });
-});*/
+popapTwo.classList.remove('open');
 
-// Анимация сообщения о результате
-var showmsg = new TimelineMax();
-showmsg.add(TweenMax.to(".msg", 0.7, {opacity: 1,y: -40,ease: Expo.easeOut}));
-showmsg.add(TweenMax.to(".msg", 0.7, {opacity: 0,y: 0,ease: Expo.easeOut,delay: 2}));
-showmsg.pause();
-
-// Анимация плашки слова "подождите"
-var loadanim = TweenLite.to(".loading", 1, {autoAlpha: 0.8});
-loadanim.pause();
-
-// Отправка данных на сервер
-$('#form').trigger('reset');
-$(function() {
-  'use strict';
-  $('#form').on('submit', function(e) {
-    $('.msg').removeClass('error success');
-    $('input').removeClass('inputerror');
-    loadanim.play();
-    e.preventDefault();
-    $.ajax({
-      url: 'send.php',
-      type: 'POST',
-      contentType: false,
-      processData: false,
-      data: new FormData(this),
-      success: function(msg) {
-        console.log(msg);
-        if (msg == 'ok') {
-          $('#form').trigger('reset');
-          $('.msg').text('Сообщение успешно отправлено').addClass('success');
-          showmsg.restart();loadanim.duration(0.3).reverse();
-        } else {
-          if (msg == 'mailerror') {
-            $("#email").addClass('inputerror');
-          }
-          $('.msg').text('Ошибка. Сообщение не отправлено').addClass('error');
-          showmsg.restart();loadanim.duration(0.3).reverse();
-        }
-      }
-    });
-  });
+openPopapTwo.addEventListener('click', function(evet) {
+  evet.preventDefault();
+  popapTwo.classList.add('open');
 });
+
+closePopapTwo.addEventListener('click', function(evet) {
+  evet.preventDefault();
+  popapTwo.classList.remove('open');
+});
+
